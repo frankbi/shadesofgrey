6c78d48d33930e0365e61b7b26c3c35216a59023
root <root@ip-172-31-45-137.ec2.internal>
2015-02-17 02:42:41 +0000

    misspelled post

--- shg.py
+++ shg.py
@@ -22,6 +22,7 @@ def add_numbers():
     )
 
 if __name__ == '__main__':
-	# post = int(os.environ.get("PORT", 5000))
-	# app.run(debug=True, host='0.0.0.0', port=port)
-	app.run()
+	port = int(os.environ.get("PORT", 8000))
+	app.run(debug=True, host='0.0.0.0', port=port)
+	# app.run(host='0.0.0.0')
+	#app.run()
